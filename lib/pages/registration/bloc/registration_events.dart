d0acd3c029f258e87b48a72eba53d765f2351051
Данил Мелихов <melihovdanil8@gmail.com>
2023-06-13 11:44:20 +0600

    testing 2

---
+++ lib/pages/registration/bloc/registration_events.dart
@@ -0,0 +1,23 @@
+abstract class RegistrationEvents {
+  const RegistrationEvents();
+}
+
+class UserNameEvent extends RegistrationEvents {
+  final String username;
+  UserNameEvent({required this.username});
+}
+
+class EmailEvent extends RegistrationEvents {
+  final String email;
+  EmailEvent({required this.email});
+}
+
+class PasswordEvent extends RegistrationEvents {
+  final String password;
+  PasswordEvent({required this.password});
+}
+
+class RePasswordEvent extends RegistrationEvents {
+  final String rePassword;
+  RePasswordEvent({required this.rePassword});
+}
---
+++ lib/pages/registration/bloc/registration_states.dart
@@ -0,0 +1,23 @@
+class RegistrationStates {
+  final String name;
+  final String email;
+  final String password;
+  final String rePassword;
+
+  const RegistrationStates({
+    this.name = "",
+    this.email = "",
+    this.password = "",
+    this.rePassword = "",
+  });
+
+  RegistrationStates copyWith(
+      {String? name, String? email, String? password, String? rePassword}) {
+    return RegistrationStates(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      rePassword: rePassword ?? this.rePassword,
+    );
+  }
+}
