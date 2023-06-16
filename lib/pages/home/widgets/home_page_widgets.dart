b8276c37fe9a2ad15426e5ebe9efc494f63253e2
Данил Мелихов <melihovdanil8@gmail.com>
2023-06-16 15:50:01 +0600

    added progress indicator when loading image

--- lib/pages/home/widgets/home_page_widgets.dart
+++ lib/pages/home/widgets/home_page_widgets.dart
@@ -1,5 +1,10 @@
+import 'dart:ui';
+
 import 'package:dbestblog/common/models/post.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_networkimage_2/provider.dart';
+import 'package:flutter_advanced_networkimage_2/transition.dart';
+import 'package:flutter_advanced_networkimage_2/zoomable.dart';
 
 Widget postGrid(PostObj item) {
   return Container(
@@ -19,9 +24,38 @@ Widget postGrid(PostObj item) {
         ]),
         Container(
           height: 300,
-          child: Image(
-            image: NetworkImage(item.image_link!),
+          child: TransitionToImage(
+            image: AdvancedNetworkImage(
+              item.image_link!,
+              timeoutDuration: const Duration(seconds: 30),
+              retryLimit: 1,
+            ),
             fit: BoxFit.cover,
+            placeholder: Container(
+              width: 300.0,
+              height: 300.0,
+              color: Colors.transparent,
+              child: const Icon(Icons.refresh),
+            ),
+            imageFilter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            width: 300.0,
+            height: 300.0,
+            enableRefresh: true,
+            loadingWidgetBuilder: (
+              context,
+              progress,
+              imageData,
+            ) {
+              // print(imageData.lengthInBytes);
+              return Container(
+                width: 300.0,
+                height: 300.0,
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(
+                  value: progress == 0.0 ? null : progress,
+                ),
+              );
+            },
           ),
         ),
         Text(item.description!),
